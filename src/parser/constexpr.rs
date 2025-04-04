bd41554e18f8f166668fda68d0c1df878fb0f62e
Yū KOBAYASHI <kb10uy@kb10uy.org>
2025-04-04 21:19:25 +0900

    はい

--- src/parser/constexpr.rs
+++ src/parser/constexpr.rs
@@ -1,1 +1,130 @@
+use chumsky::{
+    pratt::{infix, left, postfix},
+    prelude::*,
+};
 
+use crate::types::constexpr::{ConstExpr, FractionMode};
+
+pub(super) fn term<'a>() -> impl Parser<'a, &'a str, ConstExpr, extra::Err<Rich<'a, char>>> + Clone {
+    int().or(expr().delimited_by(just('('), just(')')))
+}
+
+fn expr<'a>() -> impl Parser<'a, &'a str, ConstExpr, extra::Err<Rich<'a, char>>> + Clone {
+    recursive(|expr| {
+        let term = int().or(expr.delimited_by(just('('), just(')')));
+        term.pratt((
+            infix(left(3), just('*'), |x, _, y, _| {
+                ConstExpr::Multiply(Box::new(x), Box::new(y))
+            }),
+            infix(left(3), just('/'), |x, _, y, _| {
+                ConstExpr::Divide(Box::new(x), Box::new(y), None)
+            }),
+            infix(left(2), just('+'), |x, _, y, _| {
+                ConstExpr::Add(Box::new(x), Box::new(y))
+            }),
+            infix(left(2), just('-'), |x, _, y, _| {
+                ConstExpr::Subtract(Box::new(x), Box::new(y))
+            }),
+            postfix(1, fraction_mode(), |expr, f, _| match expr {
+                ConstExpr::Divide(x, y, _) => ConstExpr::Divide(x, y, Some(f)),
+                _ => expr,
+            }),
+        ))
+    })
+}
+
+fn int<'a>() -> impl Parser<'a, &'a str, ConstExpr, extra::Err<Rich<'a, char>>> + Clone {
+    text::int::<_, extra::Err<Rich<char>>>(10)
+        .from_str()
+        .unwrapped()
+        .map(ConstExpr::Number)
+}
+
+fn fraction_mode<'a>() -> impl Parser<'a, &'a str, FractionMode, extra::Err<Rich<'a, char>>> + Clone {
+    one_of("FCUR").map(|c| match c {
+        'F' => FractionMode::Floor,
+        'C' | 'U' => FractionMode::Ceil,
+        'R' => FractionMode::Round,
+        otherwise => unreachable!("unexpected fraction mode: {otherwise}"),
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use chumsky::Parser;
+    use pretty_assertions::assert_eq;
+
+    use crate::types::constexpr::ConstExpr;
+
+    use super::{expr, term};
+
+    #[test]
+    fn constexpr_parses_basic() {
+        let parser = term();
+        assert_eq!(parser.parse("42").into_result(), Ok(ConstExpr::Number(42)));
+        assert_eq!(parser.parse("(12345)").into_result(), Ok(ConstExpr::Number(12345)));
+        assert_eq!(
+            parser.parse("1+1").into_result(),
+            Ok(ConstExpr::Add(
+                Box::new(ConstExpr::Number(1)),
+                Box::new(ConstExpr::Number(1)),
+            ))
+        );
+        assert_eq!(
+            parser.parse("2-2").into_result(),
+            Ok(ConstExpr::Subtract(
+                Box::new(ConstExpr::Number(2)),
+                Box::new(ConstExpr::Number(2)),
+            ))
+        );
+        assert_eq!(
+            parser.parse("3*3").into_result(),
+            Ok(ConstExpr::Multiply(
+                Box::new(ConstExpr::Number(3)),
+                Box::new(ConstExpr::Number(3)),
+            ))
+        );
+        assert_eq!(
+            parser.parse("4/4").into_result(),
+            Ok(ConstExpr::Divide(
+                Box::new(ConstExpr::Number(4)),
+                Box::new(ConstExpr::Number(4)),
+                None,
+            ))
+        );
+    }
+
+    #[test]
+    fn constexpr_parses_expr() {
+        let parser = expr();
+        assert_eq!(
+            parser.parse("1+1").into_result(),
+            Ok(ConstExpr::Add(
+                Box::new(ConstExpr::Number(1)),
+                Box::new(ConstExpr::Number(1)),
+            ))
+        );
+        assert_eq!(
+            parser.parse("2-2").into_result(),
+            Ok(ConstExpr::Subtract(
+                Box::new(ConstExpr::Number(2)),
+                Box::new(ConstExpr::Number(2)),
+            ))
+        );
+        assert_eq!(
+            parser.parse("3*3").into_result(),
+            Ok(ConstExpr::Multiply(
+                Box::new(ConstExpr::Number(3)),
+                Box::new(ConstExpr::Number(3)),
+            ))
+        );
+        assert_eq!(
+            parser.parse("4/4").into_result(),
+            Ok(ConstExpr::Divide(
+                Box::new(ConstExpr::Number(4)),
+                Box::new(ConstExpr::Number(4)),
+                None,
+            ))
+        );
+    }
+}
--- src/parser/query.rs
+++ src/parser/query.rs
@@ -2,10 +2,6 @@ use chumsky::prelude::*;
 
 use crate::types::query::{QueryKind, RangeQuery};
 
-pub(super) fn int<'a>() -> impl Parser<'a, &'a str, usize, extra::Err<Rich<'a, char>>> {
-    text::int::<_, extra::Err<Rich<char>>>(10).from_str().unwrapped()
-}
-
 pub(super) fn range_query<'a>() -> impl Parser<'a, &'a str, RangeQuery, extra::Err<Rich<'a, char>>> {
     query_kind().then(int()).map(|(kind, value)| RangeQuery { kind, value })
 }
